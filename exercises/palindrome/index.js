e6e805051e7afc6e2654c78a1ba9f3ae557e2c29
Uche Ozoemena <ucheozoemena@gmail.com>
2019-01-31 15:21:11 +0100

    Solve palindromes with my preferred solution

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,32 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  // solution one
+  // reverse the string and check its equality to the initial string
+  // complexity: same as that of string reversal function
+  // this assumes that string comparison is not an expensive process
+  // so if the reversal if n/2 like the one I did, same for this
+  // if string comparison is expensive, something better is needed
+
+  // solution two
+  // split the string in half, reverse one half and compare it to the
+  // other half
+  // this guarantees that string comparison is only on half the
+  // string
+  // Can we remove one step though? Maybe make it only string
+  // comparison
+
+  // solution three
+  // iterate through floor of half the length of the string and
+  // compare chars in both halves of the string
+  // compare the char @ an index with the one @ length - index - 1
+  const halfLength = Math.floor(str.length / 2);
+  for (var i = 0; i < halfLength; i++) {
+    if (str[i] !== str[str.length - i - 1]) return false;
+  }
+
+  return true;
+}
 
 module.exports = palindrome;

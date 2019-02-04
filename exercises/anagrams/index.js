d2d6e5f1bd90f85354e6eba8f8e809156cbc569e
Uche Ozoemena <ucheozoemena@gmail.com>
2019-02-04 06:34:02 +0100

    Solve anagrams with Stephen's second solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -42,26 +42,38 @@ function anagrams(stringA, stringB) {
   // return true;
 
   // Stephen's first solution
-  const aCharMap = buildCharMap(stringA),
-    bCharMap = buildCharMap(stringB);
+  // const aCharMap = buildCharMap(stringA),
+  //   bCharMap = buildCharMap(stringB);
+  //
+  // if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length)
+  //   return false;
+  //
+  // for (let char in aCharMap) {
+  //   if (aCharMap[char] !== bCharMap[char]) return false;
+  // }
+  //
+  // return true;
+  //
+  // function buildCharMap(str) {
+  //   const charMap = {};
+  //
+  //   for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+  //     charMap[char] = charMap[char] + 1 || 1;
+  //   }
+  //
+  //   return charMap;
+  // }
 
-  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length)
-    return false;
+  // Stephen's second solution
+  return cleanString(stringA) === cleanString(stringB);
 
-  for (let char in aCharMap) {
-    if (aCharMap[char] !== bCharMap[char]) return false;
-  }
-
-  return true;
-
-  function buildCharMap(str) {
-    const charMap = {};
-
-    for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
-      charMap[char] = charMap[char] + 1 || 1;
-    }
-
-    return charMap;
+  function cleanString(str) {
+    return str
+      .replace(/[^\w]/g, '')
+      .toLowerCase()
+      .split('')
+      .sort()
+      .join('');
   }
 }
 

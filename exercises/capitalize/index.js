88075c69aa48ee745ad119c60debffc6f737523b
Uche Ozoemena <ucheozoemena@gmail.com>
2019-02-10 07:07:27 +0100

    Solve capitalization with my second solution

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,26 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  // SOLUTION ONE
+  // run a replace on the string which takes each match and returns
+  // the capitalized version
+  // the regex: needs to find words case insensitively: (\w+)
+  // needs to be global so it finds as many as necessary: /(\w+)/g
+  // replacer needs to get this captured group: second arg of func
+  // and capitalize its first letter: toUpperCase() with [0],
+  // slice the rest and concatenate
+
+  // SOLUTION TWO
+  // similar to one but only focuses on finding the first word
+  // char that follows a word boundary
+  // regex: /\b\w/g (global to find every occurrence in string)
+  // only needs to return the capitalized match (because match
+  // is one letter)
+  // this is better because it only matches the first character
+  // after the word boundary
+  const firstLetterRgx = /\b\w/g;
+  return str.replace(firstLetterRgx, match => match.toUpperCase());
+}
 
 module.exports = capitalize;

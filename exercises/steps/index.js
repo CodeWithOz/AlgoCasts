5caa8415092a46fb439e1ec40b4ccf3a581e1f95
Uche Ozoemena <ucheozoemena@gmail.com>
2019-02-12 06:17:57 +0100

    Solve steps with my first solution

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,25 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  // SOLUTION ONE
+  // loop from 1 to n, padding the string with spaces until n chars
+  // for loop; i = 1, i <= n
+  // at each step, repeat # i times then pad to n with spaces
+  // log the outcome
+  for (var i = 1; i <= n; i++) {
+    console.log('#'.repeat(i).padEnd(n, ' '));
+  }
+
+  // SOLUTION TWO
+  // recursive
+  // start from the front
+  // conditions to check
+  // n == 1: print only one line
+  // cur pos < n: append a line
+  // replace starting string if empty
+  // else, append
+  // cur pos == n: append last line and return
+}
 
 module.exports = steps;
